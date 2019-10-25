235fdb8f37cd954626a3112180b2b266b2e7e474
Victor Ramalho <51335384+Victornunesp@users.noreply.github.com>
2019-10-24 23:51:39 -0300

    Add files via upload

--- lista3/q4/q4.cpp
+++ lista3/q4/q4.cpp
@@ -4,35 +4,42 @@
 #include <algorithm>
 #define QNT_GERADA 500
 #define INTERVALO 1000
+#define INICIO 0
+#define FIM 499
 
 using namespace std;
 
-int main(){
+int main()
+{
     int valor;
     int random;
     int cont = 0;
-    vector <int> intvet;
-    
+    vector<int> intvet;
+
     ofstream randomfile;
-    
+
     srand(time(NULL));
-    
+
     randomfile.open("random.txt");
 
-    for(int i = 0; i<QNT_GERADA; i++){
-        random = rand()%INTERVALO;
+    for (int i = 0; i < QNT_GERADA; i++)
+    {
+        random = rand() % INTERVALO;
         randomfile << random << endl;
         intvet.push_back(random);
     }
-    
-    randomfile.close();
-    
-    sort(intvet.begin(),intvet.end());
 
-    for(auto i : intvet){
-        cout << i << "     [" << cont+1 << "]" << endl;
-        cont ++;
-    }
-    
+    randomfile.close();
+
+    sort(intvet.begin(), intvet.end());
+
+    for(int valor : intvet)
+    {
+        cout << valor;
+    }cout << endl;
+
+    cout << "Menor valor: " << intvet[INICIO] << endl;
+    cout << "Maior valor: " << intvet[FIM] << endl;
+
     return 0;
 }
